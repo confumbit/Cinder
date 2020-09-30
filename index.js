18b0a380a1012085a4c518fa8ba86cb4844e212c
heatblast0044 <heatblast0044@gmail.com>
2020-09-30 17:42:33 +0530

    8

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+//html render
+app.get('/', function(request, response){
+    response.sendFile('/homepage.html');
+  });
+  
+  app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
