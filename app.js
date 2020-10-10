1ef6337b8a5675cf6e7f50fab912d1402bfe7268
heatblast0044 <heatblast0044@gmail.com>
2020-10-10 20:32:45 +0530

    ready for heroku

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // Port website will run on
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 
 // *** GET Routes - display pages ***
 // Root Route
