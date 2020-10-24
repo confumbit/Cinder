d34323207cb46ab6c46638531a60076466d2634f
heatblast0044 <heatblast0044@gmail.com>
2020-10-24 15:23:05 +0530

    refinements

--- public/JS/upload.js
+++ public/JS/upload.js
@@ -8,6 +8,6 @@ var feedback = function(res) {
 };
 
 new Imgur({
-    clientid: '4333f3286ee9845', //You can change this ClientID
+    clientid: '4409588f10776f7', //You can change this ClientID
     callback: feedback
 });
