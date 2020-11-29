fdf461023f10720434aa956ec94112dc57f925de
heatblast0044 <heatblast0044@gmail.com>
2020-11-29 18:13:23 +0530

    added nav

---
+++ newwebsite/JS/index.js
@@ -0,0 +1,44 @@
+// for pallarax
+const pallarax = document.querySelectorAll(".pallarax");
+
+window.addEventListener("scroll", () => {
+    let scroll = window.pageYOffset;
+
+    pallarax.forEach(element => {
+        let speed = element.dataset.speed;
+        let name = element.dataset.name;
+        if (name == 'abstract-1') {
+        element.style.transform = `translate(${scroll * speed}px)`;
+        } 
+        else {
+            element.style.transform = `translate(0, ${scroll * speed}px)`;
+        }
+    })
+})
+
+//navbar
+const navbg = document.querySelector('nav');
+const links = document.querySelectorAll('#tabs');
+const logo = document.querySelector('nav img');
+
+function openNav(e) {
+    navbg.style.backgroundColor = 'black';
+    links.forEach(function (link) {
+        link.classList.toggle('fadeIn');
+    })
+    navbg.classList.toggle('open');
+    e.target.removeEventListener('click', openNav);
+    e.target.addEventListener('click', closeNav);
+}
+
+function closeNav(e) {
+    navbg.style.backgroundColor = 'transparent';
+    links.forEach(function (link) {
+        link.classList.toggle('fadeOut');
+    })
+    navbg.classList.toggle('close');
+    e.target.removeEventListener('click', closeNav);
+    e.target.addEventListener('click', openNav);
+}
+
+logo.addEventListener('click', openNav);
